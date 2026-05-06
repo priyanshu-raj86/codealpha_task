8b98dd86ca64c07d76bd06a98946900fe1898786
Priyanshu Raj <priyanshu86780@gmail.com>
2026-05-06 17:46:36 +0530

    Initial commit

---
+++ Portfolio/scrit.js
@@ -0,0 +1,9 @@
+  
+  var typed = new Typed('#element1', {
+      strings: ['Priyanshu Raj'],
+      typeSpeed: 50,
+    });
+    var typed = new Typed('#element', {
+      strings: ['Web Developer'],
+      typeSpeed: 50,
+    });
